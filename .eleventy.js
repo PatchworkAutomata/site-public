fe989902f69cd8443e7b6033c6c2a6755327c55e
Steven Lehrburger <lehrburger@gmail.com>
2022-05-11 15:34:56 -0700

    Passthrough _redirects file to _site to fix prod on Netlify

--- .eleventy.js
+++ .eleventy.js
@@ -14,6 +14,7 @@ module.exports = function (eleventyConfig) {
       fs.writeFileSync(redirectsFilename, _redirects);
     }
   });
+  eleventyConfig.addPassthroughCopy("_redirects");
   eleventyConfig.setUseGitIgnore(false);
   return {
     dir: {
