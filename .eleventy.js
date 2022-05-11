a9e6cd20294633f7a0a02987aced97c556dd02a0
Steven Lehrburger <lehrburger@gmail.com>
2022-05-10 21:57:35 -0700

    Use _redirects file instead of netlify.toml
    This avoids committing information from the content-private submodule in the parent repo.

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,18 @@
 const { EleventyServerlessBundlerPlugin } = require("@11ty/eleventy");
+const fs = require("fs");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(EleventyServerlessBundlerPlugin, {
     name: "dynamic",
     functionsDir: "./netlify/functions/",
+    redirects: function(name, outputMap) {
+      let _redirects = "";
+      for (const [key, value] of Object.entries(outputMap)) {
+        _redirects += `${key} /.netlify/functions/${name}\n`
+      }
+      let redirectsFilename = "./_redirects";
+      fs.writeFileSync(redirectsFilename, _redirects);
+    }
   });
   eleventyConfig.setUseGitIgnore(false);
   return {
