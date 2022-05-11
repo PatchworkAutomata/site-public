PatchworkAutomata/site-public

commit 5226c7b32e27a02de32c333184b9fdcd8ebbc22a

Rework submodule merging to use node packages
This isn't working locally and I have no idea why, so I guess I'm going to push it and test in production :(

Also, I'm not sure where the changes to netlify.toml are coming from. Should this even be in the repo?

.eleventy.js
@@ -7,7 +7,7 @@ module.exports = function (eleventyConfig) {
   });
   return {
     dir: {
-      input: "content",
+      input: "content-merged",
       output: "_site",
     },
     dataTemplateEngine: "liquid",

.eleventyignore
@@ -1,3 +1,4 @@
 netlify
+content-public
 content-private
-content/README.md
+content-merged/README.md

.gitignore
@@ -1,5 +1,6 @@
 .env
 node_modules
+content-merged
 _site
 netlify/functions/dynamic/**
 !netlify/functions/dynamic/index.js

netlify.toml
@@ -3,22 +3,8 @@ publish = "_site"
 command = "npm run build"
 
 [[redirects]]
-from = "/guides/guide-draft/"
-to = "/.netlify/functions/dynamic"
-status = 200
-force = true
-_generated_by_eleventy_serverless = "dynamic"
-
-[[redirects]]
 from = "/"
-to = "/.netlify/functions/dynamic"
-status = 200
-force = true
-_generated_by_eleventy_serverless = "dynamic"
-
-[[redirects]]
-from = "/"
-to = "/"
+to = "/secure/"
 status = 302
 force = true
 

commit ceaa94e9d92c679827dce4cf835633c253f2692e

Fix issue with redirects introduced in 5226c7b32e27a02de32c333184b9fdcd8ebbc22a

.eleventy.js
@@ -8,7 +8,7 @@ module.exports = function (eleventyConfig) {
     redirects: function(name, outputMap) {
       let _redirects = "";
       for (const [key, value] of Object.entries(outputMap)) {
-        _redirects += `${key} /.netlify/functions/${name}\n`
+        _redirects += `${key} /.netlify/functions/${name} 200\n`
       }
       let redirectsFilename = "./_redirects";
       fs.writeFileSync(redirectsFilename, _redirects);

netlify/functions/dynamic/index.js
@@ -26,7 +26,6 @@ async function handler(event) {
     let oauth = new OAuth(provider);
     user = await oauth.getUser(authToken);
     org = await oauth.getOrgMembership(authToken, user.login);
-    console.log(org);
   } catch(e) {
     authError = e;
   }