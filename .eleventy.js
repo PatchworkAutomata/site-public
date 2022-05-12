3a9c56375144f3932cc947e07de45b8ca7b13372
Steven Lehrburger <lehrburger@gmail.com>
2022-05-12 12:38:31 -0700

    WIP (with infinite loop) trying to merge directories before building
    See https://stackoverflow.com/q/72221241/551814 for more info about the problem.

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,5 @@
 const { EleventyServerlessBundlerPlugin } = require("@11ty/eleventy");
-const fs = require("fs");
+const fs = require("fs-extra");
 
 const functionsDir = "netlify/functions"
 const inputDir = "content-merged";
@@ -9,6 +9,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(EleventyServerlessBundlerPlugin, {
     name: "dynamic",
     functionsDir: `./${functionsDir}/`,
+    //inputDir: `${inputDir}/-serverless`,
     redirects: function(name, outputMap) {
       let redirects = "";
       for (const [key, value] of Object.entries(outputMap)) {
@@ -22,6 +23,25 @@ module.exports = function (eleventyConfig) {
     }
   });
   eleventyConfig.setUseGitIgnore(false);
+
+  // eleventyConfig.setWatchThrottleWaitTime(5000);
+  eleventyConfig.addWatchTarget("content-private");
+  eleventyConfig.addWatchTarget("content-public");
+  eleventyConfig.on('eleventy.before', async () => {
+    console.log("");
+    console.log("");
+    console.log("eleventy.before");
+    fs.copySync('content-private', inputDir);
+    fs.copySync('content-public', inputDir);
+    //fs.copySync('content-private', `${inputDir}-serverless`);
+    //fs.copySync('content-public', `${inputDir}-serverless`);
+    console.log('merged!')
+  });
+  eleventyConfig.on('eleventy.after', async () => {
+    console.log("eleventy.after");
+    //fs.rmSync(inputDir, { recursive: true });
+    //console.log('removed!')
+  });
   return {
     dir: {
       input: inputDir,
